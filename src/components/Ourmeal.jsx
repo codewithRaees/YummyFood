6da0855e6834917ce3799b94612f2d3927b9584e
Raees <mraiskundi@gmail.com>
2024-02-12 08:09:41 +0500

    12Feb2024

--- src/components/Ourmeal.jsx
+++ src/components/Ourmeal.jsx
@@ -9,21 +9,27 @@ const Ourmeal = () => {
           Our Meal
         </h1>
         <div className=" grid md:grid-cols-2 sm:grid-cols-4 lg:grid-cols-4 gap-5">
-          {mealData.map((item) => (
-            <div className="flex flex-col duration-300 ease-out hover:scale-105">
-              <img
-                src={item.image}
-                alt={item.name}
-                className=" object-cover rounded-md   h-[200px] w-full"
-              />
+          {mealData.map((item) => {
+            const str = item.name;
+            const result = str.charAt(0).toUpperCase() + str.slice(1);
 
-              <div className="flex justify-between px-2 py-1">
-                <p className="bg-orange-500 border-8 h-18 w-18 px-3 text-white py-4 rounded-full -mt-10">
-                  {item.price}
-                </p>
+            return (
+              <div className="flex flex-col duration-300 ease-out hover:scale-105">
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className=" object-cover rounded-md   h-[200px] w-full"
+                />
+
+                <div className="flex justify-between px-2 py-1">
+                  <p className="font-bold">{result}</p>
+                  <p className="bg-orange-500 border-8 h-18 w-18 px-3 text-white py-4 rounded-full -mt-10">
+                    {item.price}
+                  </p>
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </div>
